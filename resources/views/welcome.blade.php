71fe5d217609e05f220ce6a2f4359f1f4f5d8908
pinbellerine <kevinpriambudi6@gmail.com>
2024-03-16 13:22:58 +0700

    first init github

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,2 @@
+<h1>SELAMAT DATANG DI SMK TELKOM PWT</h1>
+<a href="/pendapatan">pedapatan</a>
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\HitungController;
+use App\Http\Controllers\TestController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
+
+Route::get('/pendapatan', function () {
+    return view('pendapatan');
+})->name('home');
+
+Route::get('/', [HitungController::class, 'hitung']);
+
+Route::get('/daftar', [TestController::class, 'daftar']);
+Route::post('/kirim', [TestController::class, 'kirim']);
+
+Route::get('/dashboard', [DashboardController::class,'index']);
